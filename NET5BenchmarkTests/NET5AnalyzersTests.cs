b9aec324f81f512ff7b219d23622983c88d4dcae
Kendra Havens <kehavens@microsoft.com>
2020-10-20 09:25:05 -0700

    add file header

--- NET5BenchmarkTests/NET5AnalyzersTests.cs
+++ NET5BenchmarkTests/NET5AnalyzersTests.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿// Custom file header. Copyright and License info.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 class NET5AnalyzersTests
 {
@@ -26,17 +28,18 @@ class NET5AnalyzersTests
     public void M1()
     {
         int length = 3;
-        
+        Span<int> numbers = stackalloc int[length]; // CA2014
+
         for (int i = 0; i < length; i++)
         {
-            Span<int> numbers = stackalloc int[length]; // CA2014
+
             numbers[i] = i;
         }
     }
 
     public void M2(string str)
     {
-        ReadOnlySpan<char> slice = str[1..3]; // CA1831
+        ReadOnlySpan<char> slice = str.AsSpan()[1..3]; // CA1831
     }
 
     public void M3()
@@ -50,7 +53,7 @@ class NET5AnalyzersTests
             // probably logging some info here...
 
             // Re-throwing caught exception changes stack information
-            throw ex; // CA2200
+            throw; // CA2200
         }
     }
 }
