216c17e5a5a2caa48fd098f1c27ecf250ff5a843
Kendra Havens <kehavens@microsoft.com>
2020-11-03 09:41:17 -0800

    add linux testing

---
+++ OSInformation.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using Xunit;
+
+namespace XUnitTests
+{
+    public class RunningOnOS
+    {
+        [Fact]
+        public void IsRunningOnLinux() {
+            Assert.True(RuntimeInformation.IsOSPlatform(OSPlatform.Linux));
+        }
+
+        [Fact]
+        public void IsRunningOnWindows()
+        {
+            Assert.True(RuntimeInformation.IsOSPlatform(OSPlatform.Windows));
+        }
+    }
+}
